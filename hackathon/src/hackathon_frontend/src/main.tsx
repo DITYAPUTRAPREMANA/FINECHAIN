57aec5a0270e4f768d854fb26ec8481ed6104fc1
GustuRyan <rian.putra2003@gmail.com>
2025-06-16 15:02:01 +0800

    feat: main layout for all pages

--- hackathon/src/hackathon_frontend/src/main.tsx
+++ hackathon/src/hackathon_frontend/src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import Template from './layout/main'
+import HeroDashboard from './sections/hero'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <Template>
+        <div></div>
+    </Template>
   </React.StrictMode>
 )
