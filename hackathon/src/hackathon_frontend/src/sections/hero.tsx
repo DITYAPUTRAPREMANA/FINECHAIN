5a0032c91769f51c0be14933a1ee0ac607197982
GustuRyan <rian.putra2003@gmail.com>
2025-06-19 15:03:18 +0800

    fix: fixing hero section display and can responsive view

--- hackathon/src/hackathon_frontend/src/sections/hero.tsx
+++ hackathon/src/hackathon_frontend/src/sections/hero.tsx
@@ -1,18 +1,15 @@
 
 const HeroDashboard = () => {
     return (
-        <div className="relative w-full h-[calc(100vh-80px)]">
-            <div className="absolute flex right-0 top-0 w-full h-full z-30">
-                <img src="/images/finechain_hero.png" className="w-full h-full object-cover" />
-            </div>
-            <div className="relative z-40 w-full h-full px-20 flex justify-between items-center bg-black/25">
-                <div className="space-y-10 text-white">
-                    <h1 className="text-[68px] font-bold leading-[1]">
+        <div className="relative w-full h-[calc(100vh-80px)] bg-[url('/images/finechain_hero.png')] bg-cover">
+            <div className="relative z-40 w-full h-full px-8 md:px-20 flex justify-center lg:justify-between items-center bg-black/25">
+                <div className="flex flex-col justify-center lg:justify-start items-center lg:items-start gap-12 text-white">
+                    <h1 className="text-[40px] md:text-7xl text-center lg:text-left font-bold leading-[1]">
                         Next Generation <br/>
                         of Traffic Fine <br/>
                         Payments Collection
                     </h1>
-                    <p className="text-lg w-[520px]">
+                    <p className="text-lg text-center lg:text-left w-full lg:w-[420px] xl:w-[520px]">
                         Paying your ticket fines is now easier, faster, and more secure with blockchain technology. No more long queues, lost receipts, or doubts about where your money goes—each transaction is encrypted, transparent, and tamper-proof. 
                     </p>
                     <a href="" className="group relative w-fit px-10 py-2 flex items-center justify-center bg-[#FEF7EF] overflow-hidden border-2 border-white rounded-xl font-bold text-xl shadow-lg transition-all duration-500 hover:-translate-y-0.5">
@@ -20,9 +17,9 @@ const HeroDashboard = () => {
                         <div className="absolute left-0 -top-20 w-full h-80 bg-[linear-gradient(90deg,_#FEF7EF_18.75%,_#F4D06F_67.31%,_#F81_92.31%)] transition-all duration-500 ease-in-out group-hover:rotate-180"></div>
                     </a>
                 </div>
-                <div className="relative pr-10">
-                    <div className="relative w-[400px] h-[400px] rounded-full bg-orange-400 z-50 border-2 border-white/25 shadow-lg"></div>
-                    <div className="absolute -right-0 -bottom-12 w-[400px] h-[400px] rounded-full bg-orange-400/35 backdrop-blur-lg border-2 border-white/25 shadow-lg"></div>
+                <div className="relative pr-10 hidden lg:block">
+                    <div className="relative w-80 h-80 xl:w-[400px] xl:h-[400px] rounded-full bg-orange-400 z-50 border-2 border-white/25 shadow-lg"></div>
+                    <div className="absolute -right-0 -bottom-12 w-80 h-80 xl:w-[400px] xl:h-[400px] rounded-full bg-orange-400/35 backdrop-blur-lg border-2 border-white/25 shadow-lg"></div>
                 </div>
             </div>
         </div>
