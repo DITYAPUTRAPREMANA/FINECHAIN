4a11d118c9db8bbbcdaff69e567bdc3a83db5841
stevenchan7 <0stevenchan0@gmail.com>
2025-06-20 08:50:06 +0800

    feat: add payment main page responsive

--- hackathon/src/hackathon_frontend/src/components/payment/TicketNumberInput.tsx
+++ hackathon/src/hackathon_frontend/src/components/payment/TicketNumberInput.tsx
@@ -1,14 +1,14 @@
 export default function TicketNumberInput() {
   return (
-    <div className="w-full rounded-xl overflow-hidden bg-white/20 backdrop-blur-sm">
-      <div className="bg-navy text-center text-white font-bold p-2">
+    <div className="w-full max-w-lg rounded-xl overflow-hidden bg-white/20 backdrop-blur-sm">
+      <div className="bg-navy text-lg text-center text-white font-bold px-4 py-3">
         <p>Enter ticket number</p>
       </div>
-      <form action="" className="px-4 py-8">
+      <form action="" className="w-full p-4">
         <input
           id="ticket-number"
           type="text"
-          className="w-full bg-white rounded-full px-4 py-2 text-base text-navy focus-within:outline-navy"
+          className="w-full bg-white rounded-full px-4 py-3 text-base text-navy focus-within:outline-navy"
           placeholder="ticket number..."
         />
       </form>
