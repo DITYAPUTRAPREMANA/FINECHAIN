4180014756e4458e9e34fe8911ca854e2af7a1f0
GustuRyan <rian.putra2003@gmail.com>
2025-06-27 16:00:02 +0800

    fix: update website structure

--- hackathon/src/hackathon_frontend/src/main.tsx
+++ hackathon/src/hackathon_frontend/src/main.tsx
@@ -3,29 +3,35 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Template from './layout/main'
 import PaymentPage from './pages/PaymentPage'
-import HeroDashboard from './sections/hero'
-import FinesDashboard from './sections/fines'
+import HeroDashboard from './components/dashboard/hero'
+import FinesPage from './pages/FinesPage'
 import TicketPaymentCheckoutPage from './pages/TicketPaymentCheckoutPage'
 import PaymentHistoryDetail from './pages/PaymentHistoryDetail'
+import FeatureDashboard from './components/dashboard/features'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <Template>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Template>
         <Routes>
-          <Route path="/" element={<HeroDashboard />} />
+          <Route path="/" element={
+            <>
+              <HeroDashboard />
+              <FeatureDashboard />
+            </>
+          } />
+          <Route path="/fines" element={<FinesPage />} />
           <Route path="/payments" element={<PaymentPage />} />
+          <Route path="/history/:id" element={<PaymentHistoryDetail />} />
           <Route
             path="/payments/checkout"
             element={<TicketPaymentCheckoutPage />}
           />
-          <Route path="/fines" element={<FinesDashboard />} />
-          <Route path="/history" element={<PaymentHistoryDetail />} />
         </Routes>
-      </BrowserRouter>
-    </Template>
+      </Template>
+    </BrowserRouter>
   </React.StrictMode>
 )
