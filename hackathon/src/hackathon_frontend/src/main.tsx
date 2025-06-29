9cdd170be31a79353bc81072ee32a9bc1a1eccca
Trizz__ <kihajar869@gmai.com>
2025-06-29 21:38:21 +0800

    update

--- hackathon/src/hackathon_frontend/src/main.tsx
+++ hackathon/src/hackathon_frontend/src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="475841752276-if3iv5ql70nhh073ssjr66so8pu9ivd2.apps.googleusercontent.com">
     <App />
-    </GoogleOAuthProvider>;
   </React.StrictMode>
 )
